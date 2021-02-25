d910ad1d05c3c82cfa5b67cdee0cd39f42aa9984
Tapio Jokinen <tapio.jokinen@hotmail.com>
2021-02-25 17:24:06 +0200

    Function to download and modify traffic data

---
+++ flaskr/traffic_data.py
@@ -0,0 +1,53 @@
+"""
+File: traffic_data.py
+Author: Tapio Jokinen
+
+Description:
+Downloads counters-15min.csv file which contains data of traffic volumes for cars, bicycles and pedestrians.
+Creates a new file which contains traffic volume data of certain type of a vehicle. The vehicle type
+is defined as function parameter.
+"""
+
+import pandas as pd
+import json
+import time
+
+def traffic_data(vehicle_type):
+
+    if vehicle_type != ("car" or "bicycle" or "pedestrian"):
+        print("Vehicle type error")
+        return
+
+    # Read file into pandas dataframe
+    try:
+        df = pd.read_csv("https://dev.turku.fi/datasets/ecocounter/2020/counters-15min.csv", sep=",", na_values=0)
+    except (pd.errors.EmptyDataError, pd.errors.ParserError):
+        print("Error while reading file into dataframe.")
+
+    # Delete unnecessary columns from the dataframe
+    for column in df.columns:
+        if vehicle_type == "car":
+            if not column.endswith(("aika", "AP", "AK")):
+                del df[column]
+        elif vehicle_type == "bicycle":
+            if not column.endswith(("aika", "PP", "PK")):
+                del df[column]
+        elif vehicle_type == "pedestrian":
+            if not column.endswith(("aika", "JP", "JK")):
+                del df[column]
+
+
+    file_name = "./static/data/traffic_data_{}.json".format(vehicle_type)
+
+    # Convert object to a json file.
+    df.to_json(file_name, orient="records")
+
+    # Make json file more readable for human eyes.
+    with open(file_name) as f:
+        data = json.load(f)
+
+    jsonString = json.dumps(data, indent=2)
+
+    k = open(file_name, "w")
+    k.write(jsonString)
+    k.close()
