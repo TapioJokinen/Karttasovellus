37bc7c35c41aa119aa961b4a58ec10316b6f2d19
Tapio Jokinen <tapio.jokinen@hotmail.com>
2021-02-26 17:02:53 +0200

    Initialization for cars-function

--- flaskr/static/js/googleMaps.js
+++ flaskr/static/js/googleMaps.js
@@ -10,11 +10,12 @@ let defaultCenter = { lat: 60.44600, lng: 22.2646300};
 let defaultZoom = 13;
 
 const initMap = async () => {
-  console.log("haista")
     map = new google.maps.Map(document.getElementById("map"), {
         center: defaultCenter,
         zoom: defaultZoom,
       });
 }
 
-initMap()
+initMap().then(() => {
+  cars(map)
+})
