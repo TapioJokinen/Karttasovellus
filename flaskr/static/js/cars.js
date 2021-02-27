cd3d271ac1efc0b58672a7dd9ef0f0eeb2349f4c
Tapio Jokinen <tapio.jokinen@hotmail.com>
2021-02-27 11:51:22 +0200

    Added search feature

--- flaskr/static/js/cars.js
+++ flaskr/static/js/cars.js
@@ -57,4 +57,65 @@ const cars = async(map) => {
         }
     });
 
+    btnSearchCars.addEventListener("click", () => {
+
+        // Clear old data and error texts.
+        dataCars.innerHTML = "";
+        errorText.textContent = "";
+
+        let dateStart = document.getElementById("date-start-cars").value;
+        let dateEnd = document.getElementById("date-end-cars").value;
+        let timeStart = document.getElementById("time-start-cars").value;
+        let timeEnd = document.getElementById("time-end-cars").value;
+
+        // Fix time values to be divisible by 15.
+        timeStart = fixTime(timeStart);
+        timeEnd = fixTime(timeEnd);
+
+        // Add seconds to time string.
+        let startDateString = `${dateStart} ${timeStart}:00`;
+        let endDateString = `${dateEnd} ${timeEnd}:00`;
+
+        // Full date variables.
+        let start = new Date(startDateString);
+        let end = new Date(endDateString);
+
+        // Check for errors then proceed.
+        if (start > end) {
+            errorText.textContent = "Start date must be smaller than the end date!";
+        } else {
+            (async () => {
+
+                // Fetch location and traffic data.
+                let locationDataResponse = await fetch("/data/liikennelaskimet");
+                let trafficDataResponse = await fetch("/data/car");
+    
+                // If response ok(200) convert response to json.
+                if (locationDataResponse.ok && trafficDataResponse.ok) {
+                    locationData = await locationDataResponse.json();
+                    trafficData = await trafficDataResponse.json();
+                } else {
+                    throw new Error(`HTTP error!
+                        \nlocationDataResponse: ${locationDataResponse.status}
+                        \ntrafficDataResponse: ${trafficDataResponse.status}` );
+                }
+                
+                // Draw areas on map
+                drawnMapObjects = drawAreasOnMap(map, filterLocations(locationData, trafficData), "car");
+
+                // Total traffic between the start and end date.
+                let totalTraffic = totalTraffic(trafficData, start, end);
+
+                Object.keys(totalTraffic).forEach((key) => {
+                    let s = `${key}: ${totalTraffic[key]}`;
+                    let ul = document.getElementById("data-cars");
+                    let li = document.createElement("li");
+                    li.appendChild(document.createTextNode(s));
+                    ul.appendChild(li);
+                  });
+            })();
+        }
+
+    });
+
 }
