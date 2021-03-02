64eabe7f3957483e2ba168c420134785f3937916
Tapio Jokinen <tapio.jokinen@hotmail.com>
2021-03-02 10:58:27 +0200

    Fixed NaN values

--- flaskr/static/js/totalTraffic.js
+++ flaskr/static/js/totalTraffic.js
@@ -2,13 +2,14 @@
 Author: Tapio Jokinen
 
 Description:  Calculates the total traffic between dateStart and dateEnd. Returns a dictionary
-where key/value pair is { "place": "total traffic value"}
+where key/value pair is { "place": "total traffic volume"}
 */
 
 const totalTraffic = (trafficData, dateStart, dateEnd) => {
 
   // Get the names of the places.
   let places = Object.keys(trafficData[0]);
+  console.log(places)
 
   // Contains values of total traffic for each place.
   let countList = new Array(places.length - 1).fill(0);
@@ -18,16 +19,22 @@ const totalTraffic = (trafficData, dateStart, dateEnd) => {
 
     // Find the start date.
     if (dateStart.getTime() === time.getTime()) {
+     
       for (let j = i; j < trafficData.length; j++) {
         time = new Date(trafficData[j]["aika"]);
 
         // Index 0 is "aika" so start loop at index 1.
         for (let k = 1; k < places.length; k++) {
-          countList[k - 1] = countList[k - 1] + parseInt(trafficData[j][places[k]]);
+
+          // In case on NaN value return 0.
+          let volume = parseInt(trafficData[j][places[k]]) || 0;
+
+          countList[k - 1] = countList[k - 1] + volume;
         }
 
         // If end date is reached, break.
         if (time.getTime() === dateEnd.getTime()) {
+          console.log("End time reached.")
           break;
         }
       }
